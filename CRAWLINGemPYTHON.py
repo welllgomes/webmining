96a463d973d36fb528e80c6e0c5cebf5366270d9
Wellington Gomes <104743150+welllgomes@users.noreply.github.com>
2024-06-24 21:14:59 -0300

    Update CRAWLINGemPYTHON.py

--- CRAWLINGemPYTHON.py
+++ CRAWLINGemPYTHON.py
@@ -1,18 +1,21 @@
 #lib
-#!pip install requests
-#!pip install beautifulsoup4
+import requests
 
 #code
-import requests
-from bs4 import BeautifulSoup
-
-def obter_cotacao(url, elemento, atributo):
-    x = requests.get(url)
-    if x.status_code == 200:
-        soup = BeautifulSoup(x.content, 'html.parser')
-        cotacao = soup.find(elemento, atributo)
-        if cotacao:
-            valor = cotacao.get('value').strip().replace(',', '.')
+def obter_cotacao(url, elemento_id):
+    resposta = requests.get(url)
+    if resposta.status_code == 200:
+        content = resposta.text
+        # Posição inicial do elemento pelo ID
+        pos_inicio = content.find(f'id="{elemento_id}"')
+        if pos_inicio != -1:
+            # Ponto onde o id é encontrado
+            snippet = content[pos_inicio:]
+            # Encontra o início e o fim do valor
+            pos_inicio_valor = snippet.find('value="') + 7
+            pos_fim_valor = snippet.find('"', pos_inicio_valor)
+            valor = snippet[pos_inicio_valor:pos_fim_valor]
+            valor = valor.strip().replace(',', '.')
             return round(float(valor), 2)
     return None
 
@@ -20,12 +23,12 @@ def main():
     url_bitcoin = 'https://dolarhoje.com/bitcoin-hoje/'
     url_dolar = 'https://dolarhoje.com/'
     
-    valor_btc = obter_cotacao(url_bitcoin, 'input', {'id': 'nacional'})
-    valor_dol = obter_cotacao(url_dolar, 'input', {'id': 'nacional'})
+    valor_btc = obter_cotacao(url_bitcoin, 'nacional')
+    valor_dol = obter_cotacao(url_dolar, 'nacional')
     
     if valor_btc and valor_dol:
         btc_dol = valor_btc / valor_dol
-        print(f'wellington_gomes >> 1 bitcoin (R${valor_btc}) vale US${btc_dol:.2f} (US$1 = R${valor_dol})')
+        print(f'1 bitcoin (R${valor_btc}) vale US${btc_dol:.2f} (US$1 = R${valor_dol})')
     else:
         print("Não foi possível obter as cotações necessárias.")
 
